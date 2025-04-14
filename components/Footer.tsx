257f691fda8b82c1b1f774d788d6331913fbc4da
I-Am-Fox <coldire@protonmail.com>
2025-04-14 18:44:46 +0100

    Fresh

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-center py-4 text-xs tracking-widest border-t border-gray-700">
+      © {new Date().getFullYear()} Black Veil Archives
+    </footer>
+  );
+}

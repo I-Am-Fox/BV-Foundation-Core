c7b40701cf3d081bdf04f6a7eb5c21c4549fa07e
I-Am-Fox <coldire@protonmail.com>
2025-04-14 20:07:30 +0100

    Fix Netlify CSS build error by enabling experimental css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  future: {
+    webpack5: true,
+  },
+  experimental: {
+    css: true,
+  },
+};
 
 module.exports = nextConfig;

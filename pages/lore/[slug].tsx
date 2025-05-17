ff4b3ef7a320a84c5221e2ffd00ca284af7aebde
I-Am-Fox <coldire@protonmail.com>
2025-05-17 21:45:56 +0100

    Add InterviewLog component to [slug].tsx and clean up DELTA_WolfBrothers interview log

--- pages/lore/[slug].tsx
+++ pages/lore/[slug].tsx
@@ -9,7 +9,7 @@ import HeaderSection from '../../components/LoreElements/HeaderSection';
 import WarningBox from '../../components/LoreElements/WarningBox';
 import DossierTagList from '../../components/LoreElements/DossierTagList';
 import { MdxFrontmatterSchema, MdxFrontmatter } from '../../lib/mdx-schema';
-import {InterviewLog, Action, Dialogue} from "../../components/DossierElements/InterviewLogs";
+import { InterviewLog, Action, Dialogue } from '../../components/DossierElements/InterviewLogs';
 
 interface Props {
   frontMatter: MdxFrontmatter;
@@ -20,7 +20,10 @@ export default function LoreEntry({ frontMatter, mdxSource }: Readonly<Props>) {
   return (
     <div className="bg-black text-white min-h-screen p-4">
       <div className="prose prose-invert max-w-4xl mx-auto">
-        <MDXRemote {...mdxSource} components={{ HeaderSection, WarningBox, DossierTagList, InterviewLog, Action, Dialogue }} />
+        <MDXRemote
+          {...mdxSource}
+          components={{ HeaderSection, WarningBox, DossierTagList, InterviewLog, Action, Dialogue }}
+        />
       </div>
     </div>
   );
